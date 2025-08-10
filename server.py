322504c4a93d4e074dffc7a26e6fc294f638bd66
Mathew Kadambatt <matmart5@gmail.com>
2025-08-10 23:59:41 +0530

    fix: enable the cron

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from prometheus_client import Gauge, Counter, generate_latest, CONTENT_TYPE_LATEST
 import numpy as np
 import os
+import json
 
 from train_and_sync import (
     create_model,
@@ -12,8 +13,29 @@ from train_and_sync import (
     get_weights,
     gossip_sync,
     load_uci_har_subject_data,
-    getConfigMap
 )
+
+def getConfigMap(pod_index):
+    config_file = f"/etc/feature-flags/flags-{pod_index}.json"
+    try:
+        with open(config_file) as f:
+            flags = json.load(f)
+    except FileNotFoundError:
+        print(f"Feature flags file not found for pod {pod_index}, using default flags")
+    try:    
+        with open("/etc/feature-flags/default.json") as f:
+                flags = json.load(f)
+    except FileNotFoundError:
+        print("Default feature flags file not found, using empty flags")
+        flags = {}
+    print(f"Pod {pod_index} using flags: {flags}")
+    if flags.get("useSyncTraining"):
+        print("Sync Federated Learning enabled")
+    if flags.get("enableDeepShallowFeaturesweightage"):
+        print("Deep Shallow Features weightage enabled")
+    if flags.get("enableTimeDistanceWeightage"):
+        print("Time Distance Weightage enabled")
+    return flags
 
 # Environment Setup
 POD_NAME = os.getenv("HOSTNAME")
